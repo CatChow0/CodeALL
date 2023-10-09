bb4576cb5ad451857bd66267646062f25daacbc0
Catchow0 <thetrueshibadoggo@gmail.com>
2023-10-09 12:00:39 +0200

    Commit 3

--- C++/base/main.cpp
+++ C++/base/main.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 
 int main(void) {
 
 //  affiche dans la console le nombre 5
-    int foo = 5;
-    std::cout << "foo = " << foo << std::endl;
+    // int foo = 5;
+    // std::cout << "foo = " << foo << std::endl;
 
 //  affiche dans la console le nombre qui est saisi par l'utilisateur
-    std::cin >> foo;
+    // std::cin >> foo;
+
+//  gestion de la mémoire vive
+
+    // int* foo = new int;  allocation de la mémoire vive
+    // delete foo; libération de la mémoire vive
+    // foo = nullptr; 
 
     
-
 
     return 0;
 }
