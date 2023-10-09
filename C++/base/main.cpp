3209c7bc92baa53284af530e070e8283508a8007
Catchow <thetrueshibadoggo@gmail.com>
2023-10-09 10:45:56 +0200

    First commint pc 2

---
+++ C++/base/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(void) {
+
+//  affiche dans la console le nombre 5
+    int foo = 5;
+    std::cout << "foo = " << foo << std::endl;
+
+//  affiche dans la console le nombre qui est saisi par l'utilisateur
+    std::cin >> foo;
+
+    
+
+
+    return 0;
+}
---
+++ C/revisionC/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int* p = malloc(sizeof(int));
+
+    return 0;
+}
