3e51d57dde4b5816bb91c7da69e4f798f7417cb4
Catchow0 <thetrueshibadoggo@gmail.com>
2023-10-12 10:39:24 +0200

    Pokemon Like Part5. Get Pokemon Stats OK Round Counter OK Fight Initialized OK

--- C++/proj/main.cpp
+++ C++/proj/main.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <include/progressbar.hpp>
 
 // ------------------ //
 // Class des pokemons //
@@ -82,25 +83,16 @@ void Create_Pokemon() {
 
 };
 
-// ----------------------------- //
-// Attribut le Pokemon au joueur //
-// ----------------------------- //
-
-std::string attribute_pokemon_to_player(std::string Default_Pokemon) {
-    std::string Player_Pokemon_Name = Default_Pokemon;
-
-    return Player_Pokemon_Name;
-};
-
 // ---------------------------- //
 // Renomme le Pokemon du joueur //
 // ---------------------------- //
 
 std::string rename_Pokemon(std::string Player_Pokemon) {
-    int rename_choice = 1;
+    int rename_choice = 0;
+    int valide_choice = 0;
     std::string res;
 
-    while (rename_choice == 1) {
+    while (rename_choice == 0) {
 
         std::cout << "Voulez-vous renommer votre pokemon de depart?" << std::endl << "Oui [0] || Non [1]" << std::endl;
         std::cin >> rename_choice;
@@ -115,33 +107,17 @@ std::string rename_Pokemon(std::string Player_Pokemon) {
             std::cout << "Entre le nouveau nom du pokemon" << std::endl;
             std::cin >> Player_Pokemon;
             std::cout << "Valider le choix : \"" << Player_Pokemon << "\" " << std::endl << "Oui [0] || Non [1]" << std::endl;
-            std::cin >> rename_choice;
+            std::cin >> valide_choice;
+            if (valide_choice == 0) {
+                res = Player_Pokemon;
+                rename_choice = 1;
+            };
             
         };      
     };
 
     return res;
 };
-
-// Donne les informations du pokemon
-
-// char Get_Pokemon_Info(int Id) {
-
-//     std::ifstream Pokemon_file;
-//     Pokemon_file.open("Pokemon.txt", std::ios::in);
-//     std::string info;
-
-//     for (int i = 0; i < 4;i++) {
-//         std::getline(Pokemon_file, info);
-//     };
-
-//     Character Current_Pokemon;
-
-
-
-// };
-
-
 
 // ---------------------------------------- //
 // Fonction pour choisir le premier pokemon //
@@ -186,7 +162,6 @@ int Player_pokemon_choice() {
     // ------------------------------------------------ //
 
     int end_of_choice = 1;
-    std::string Player_Pokemon_Name;
     while (end_of_choice == 1) {
         std::cout << "Entre le numeros pour choisir ton pokemon de depart" << std::endl;
         std::cout << Default_Pokemon_Name_1 << " [1]," << Default_Pokemon_Name_2 << " [2],"<< Default_Pokemon_Name_3 << " [3]" << std::endl;
@@ -196,15 +171,12 @@ int Player_pokemon_choice() {
         {
         case 1:
             std::cout << "Vous avez selectioner " << Default_Pokemon_Name_1 << " de type Feu "<< std::endl;
-            Player_Pokemon_Name = attribute_pokemon_to_player(Default_Pokemon_Name_1);
             break;
         case 2:
             std::cout << "Vous avez selectioner " << Default_Pokemon_Name_2 << " de type Eau" << std::endl;
-            Player_Pokemon_Name = attribute_pokemon_to_player(Default_Pokemon_Name_2);
             break;
         case 3:
             std::cout << "Vous avez selectioner " << Default_Pokemon_Name_3 << " de type Plante" << std::endl;
-            Player_Pokemon_Name = attribute_pokemon_to_player(Default_Pokemon_Name_3);
             break;
         default:
             break;
@@ -220,11 +192,6 @@ int Player_pokemon_choice() {
             std::cout << "Vous n'avez selectioner aucun des pokemons disponible" << std::endl;
         };
     };
-
-    // Propose au joueur de choisir le nom de son premier pokemon //
-
-    Player_Pokemon_Name = rename_Pokemon(Player_Pokemon_Name); // Réattribut le nom du pokemon au joueur
-    
 
     return player_pokemon_choice;
 };
@@ -273,16 +240,80 @@ std::string Current_Pokemon_Name(int Player_pokemon_Id) {
     };
     std::getline(Pokemon_File,Pokemon_Name, ' ');
     std::getline(Pokemon_File,Pokemon_Name);
-    std::cout << "El poutras " << Pokemon_Name << std::endl;
+    Pokemon_File.close();
 
     return Pokemon_Name;
+};
+
+int stats(int Pokemon_ID, int stats_ligne) { // stats_ligne = ligne de la stats par rapport a l'ID du pokemon
+
+    std::string Id = "Id: ";
+    std::string Pokemon_Id_String = std::to_string(Pokemon_ID);
+    std::string Id_to_find = Id + Pokemon_Id_String;
+
+    std::ifstream Pokemon_File;
+    Pokemon_File.open("Pokemon.txt", std::ios::in);
+
+    std::string Pokemon_Stats;
+    while (std::getline(Pokemon_File,Pokemon_Stats)) {
+        if (Pokemon_Stats.find(Id_to_find)!= std::string::npos) {
+            break;
+        };
+    };
+    for (int i = 0; i < stats_ligne; i++) { // Vas à la ligne de la stats voulue
+        std::getline(Pokemon_File,Pokemon_Stats);
+
+    };
+    std::getline(Pokemon_File,Pokemon_Stats,' ');
+    std::getline(Pokemon_File,Pokemon_Stats);
+    Pokemon_File.close();
+
+    int Res_Pokemon_Stats = std::stoi(Pokemon_Stats);
+
+    return Res_Pokemon_Stats;
 };
 
 // ------ //
 // Combat //
 // ------ //
 
-void fight(int Player_Starter_Id, int Enemy_Starter_Id) {
+void fight(std::string Player_Pokemon_Name, int Player_Pokemon_Health, int Player_Pokemon_Attack, int Player_Pokemon_Defense, std::string Enemy_Pokemon_Name, int Enemy_Pokemon_Health, int Enemy_Pokemon_Attack, int Enemy_Pokemon_Defense) {
+
+    int current_player_health = Player_Pokemon_Health;
+    int current_enemy_health = Enemy_Pokemon_Health;
+
+    int current_round = 0;
+    bool end_of_battle = false;
+
+    while (end_of_battle == false) {
+        
+        if (current_round == 0) {
+            std::cout << "Début du combat" << std::endl;
+        };
+        std::cout << "Tour: " << current_round + 1 << std::endl;
+
+        progressbar player_hp_bar(Player_Pokemon_Health); // Crée un barre de vie pour le joueur
+        player_hp_bar.set_todo_char(" ");
+        player_hp_bar.set_done_char("█");
+        player_hp_bar.set_opening_bracket_char("[");
+        player_hp_bar.set_closing_bracket_char("]");
+
+        progressbar enemy_hp_bar(Enemy_Pokemon_Health); // Crée un barre de vie pour l'enemie
+        enemy_hp_bar.set_todo_char(" ");
+        enemy_hp_bar.set_done_char("█");
+        enemy_hp_bar.set_opening_bracket_char("[");
+        enemy_hp_bar.set_closing_bracket_char("]");
+        
+        std::cin >> current_player_health;
+
+        if (current_player_health <= 0) {
+            end_of_battle = true;
+            std::cout << "Vous avez perdu" << std::endl;
+        };
+
+        player_hp_bar.update();
+    
+    };
 
 };
 
@@ -298,18 +329,27 @@ int main() {
 
     int Enemy_Starter_Id = Enemy_pokemon_choice(Player_Starter_Id);
 
-    std::string Player_Starter_Name = Current_Pokemon_Name(Player_Starter_Id);
-    std::cout << Player_Starter_Name << std::endl;
-    int Player_Starter_Health;
-    int Player_Starter_Attack;
-    int Player_Starter_Defense;
+    std::string Player_Starter_Name = Current_Pokemon_Name(Player_Starter_Id); // Récupère le nom du pokemon du joueur
+    Player_Starter_Name = rename_Pokemon(Player_Starter_Name); // Renomme le pokemon du joueur
+    std::cout << "Player Pokemon Name: " << Player_Starter_Name << std::endl;
+    int Player_Starter_Health = stats(Player_Starter_Id, 1); // Récupère la vie
+    std::cout << "Player Pokemon Health: " << Player_Starter_Health << std::endl;
+    int Player_Starter_Attack = stats(Player_Starter_Id, 2); // Récupère l'attaque
+    std::cout << "Player Pokemon Attack: " << Player_Starter_Attack << std::endl;
+    int Player_Starter_Defense = stats(Player_Starter_Id, 3); // Récupère la defense
+    std::cout << "Player Pokemon Defense: " << Player_Starter_Defense << std::endl;
 
-    std::string Enemy_starter_Name;
-    int Enemy_Starter_Health;
-    int Enemy_Starter_Attack;
-    int Enemy_Starter_Defense;
+    std::string Enemy_starter_Name = Current_Pokemon_Name(Enemy_Starter_Id); // Récupère le nom du pokemon de l'enemie
+    std::cout << "Enemy Pokemon Name: " << Enemy_starter_Name << std::endl;
+    int Enemy_Starter_Health = stats(Enemy_Starter_Id, 1); // Récupère la vie
+    std::cout << "Enemy Pokemon Health: " << Enemy_Starter_Health << std::endl;
+    int Enemy_Starter_Attack = stats(Enemy_Starter_Id, 2); // Récupère la attaque
+    std::cout << "Enemy Pokemon Attack: " << Enemy_Starter_Attack << std::endl;
+    int Enemy_Starter_Defense = stats(Enemy_Starter_Id, 3); // Récupère la defense
+    std::cout << "Enemy Pokemon Defense: " << Enemy_Starter_Defense << std::endl;
+
     
-
+    
 
 
     // fight(Player_Starter_Id, Enemy_Starter_Id);
