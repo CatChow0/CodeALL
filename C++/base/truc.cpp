3dce1aa32537d34a145d51ecc383b25d59738026
Catchow0 <thetrueshibadoggo@gmail.com>
2023-10-16 12:49:12 +0200

    N°36 - P2 cpp start

---
+++ C++/base/truc.cpp
@@ -0,0 +1,30 @@
+// #include <iostream>
+
+//template<typename T, typename U>
+//class Maths {
+
+// public:
+// 	T add(T a, U b) {
+// 		return a + b;
+// 	}
+
+// 	T subtract(T a, U b) {
+// 		return a - b;
+// 	}
+
+// 	T multiply(T a, U b) {
+// 		return a * b;
+// 	};
+
+// 	Void DisplayNumber(T number) {
+// 		std::cout << number << std::endl;
+// 	}
+// };
+
+// int main()
+// {
+// 	Maths<int, float> maths;
+// };
+
+// Vector2D maths a refaire
+
